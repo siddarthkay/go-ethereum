8ba8984079ac065e8b2584edc21a21b3ec7e0aec
Siddarth Kumar <siddarthkay@gmail.com>
2023-10-29 21:41:46 +0530

    update build flags to avoid x86_64 simulators

--- accounts/keystore/watch.go
+++ accounts/keystore/watch.go
@@ -14,7 +14,9 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// +build darwin,!ios,cgo freebsd linux,!arm64 netbsd solaris
+//go:build ((darwin && !ios && cgo) || freebsd || (linux && !arm64 && !android) || netbsd || solaris) && !(android && amd64)
+// +build darwin,!ios,cgo freebsd linux,!arm64,!android netbsd solaris
+// +build !android !amd64
 
 package keystore
 
